8ed6aeb1c366bcefd21e53f77e4746791dad79dd
lk <lkghost7@gmail.com>
2018-08-21 23:12:18 +0300

    Перенос всех базовых классов. Создание базовых тестов.

---
+++ test/src/itacademy/entity/AssertTestStarter.java
@@ -0,0 +1,13 @@
+package itacademy.entity;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BranchTest.class,
+        PrivilegeTest.class
+})
+public class AssertTestStarter {
+
+}
---
+++ test/src/itacademy/entity/PrivilegeTest.java
@@ -0,0 +1,11 @@
+package itacademy.entity;
+
+import org.junit.Test;
+
+public class PrivilegeTest {
+
+    @Test
+    public void createPrivilege() {
+
+    }
+}
