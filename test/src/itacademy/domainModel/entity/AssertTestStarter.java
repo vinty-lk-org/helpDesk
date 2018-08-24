ca24a01567bb7c44e7b3921f912d3f045814b298
vinty1978@gmail.com <djkxtyjr3>
2018-08-24 13:15:22 +0300

    tests

--- test/src/itacademy/domainModel/entity/AssertTestStarter.java
+++ test/src/itacademy/domainModel/entity/AssertTestStarter.java
@@ -1,12 +1,17 @@
 package itacademy.domainModel.entity;
 
+import itacademy.domainModel.dao.BranchDaoImpl;
+import itacademy.domainModel.dao.BranchDaoImplTest;
+import itacademy.domainModel.dao.SubdivisionDaoImplTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         BranchTest.class,
-        PrivilegeTest.class
+        PrivilegeTest.class,
+        SubdivisionDaoImplTest.class,
+        BranchDaoImplTest.class
 })
 public class AssertTestStarter {
 }
