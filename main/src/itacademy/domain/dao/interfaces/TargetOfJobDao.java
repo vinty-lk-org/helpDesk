fc9f459a32d6c5402f0ad3baea2a16f4976e1bbb
lk <lkghost7@gmail.com>
2018-08-27 11:30:37 +0300

    TargetOfJobDao интерфейс, TargetOfJobDaoImpl - весь круд,  заготовки для теста.

---
+++ main/src/itacademy/domain/dao/interfaces/TargetOfJobDao.java
@@ -0,0 +1,9 @@
+package itacademy.domain.dao.interfaces;
+
+import itacademy.domain.dao.common.BaseDao;
+import itacademy.domain.entity.TargetOfJob;
+
+
+public interface TargetOfJobDao extends BaseDao<TargetOfJob> {
+
+}
---
+++ test/src/itacademy/domain/dao/impl/TargetOfJobDaoImplTest.java
@@ -0,0 +1,24 @@
+package itacademy.domain.dao.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TargetOfJobDaoImplTest {
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
