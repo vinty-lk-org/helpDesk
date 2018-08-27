49d00cb0c106bd37e27698db42c3c88e8ae49945
vinty1978@gmail.com <djkxtyjr3>
2018-08-27 15:34:02 +0300

    Правка теста

--- test/src/itacademy/domain/dao/impl/SystemUserDaoImplTest.java
+++ test/src/itacademy/domain/dao/impl/SystemUserDaoImplTest.java
@@ -1,9 +1,9 @@
 package itacademy.domain.dao.impl;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import itacademy.domain.entity.Branch;
 import itacademy.domain.entity.Subdivision;
 import itacademy.domain.entity.SystemUser;
-import itacademy.domain.entity.TargetOfJob;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,37 +12,39 @@ import java.util.Optional;
 
 public class SystemUserDaoImplTest {
 
-    @Test
-    public void save() {
-        System.out.println("нужна проверка теста");
-        SystemUserDaoImpl user = SystemUserDaoImpl.getInstance();
-        SystemUser systemUser = new SystemUser();
-        systemUser.setName("Пользователь тест");
-        systemUser.setFamaly("Фамилия тест");
-        systemUser.setEmail("email test");
-        systemUser.setPassword("pass test");
-        systemUser.setBranchId(new Branch("филиал тест"));
-        systemUser.setSubdivisionId(new Subdivision("админ тест"));
-        Long id = user.save(systemUser);
-        Assert.assertNotNull(id);
-        Optional<SystemUser> optionalSystemUser = user.findById(id);
-        String nameSystemUser = "";
-        if (optionalSystemUser.isPresent()) {
-            nameSystemUser = optionalSystemUser.get().getName();
-        }
-        Assert.assertEquals(systemUser.getName(), nameSystemUser);
-        user.delete(id);
+  @Test
+  public void save() {
+    // TODO Нужна проверка теста
+    SystemUserDaoImpl user = SystemUserDaoImpl.getInstance();
+    SystemUser systemUser = new SystemUser();
+    systemUser.setName("Пользователь тест");
+    systemUser.setFamaly("Фамилия тест");
+    systemUser.setEmail("email test");
+    systemUser.setPassword("pass test");
+    systemUser.setBranchId(new Branch("филиал тест"));
+    systemUser.setSubdivisionId(new Subdivision("админ тест"));
+    Long id = user.save(systemUser);
+    Assert.assertNotNull(id);
+    Optional<SystemUser> optionalSystemUser = user.findById(id);
+    String nameSystemUser = "";
+    if (optionalSystemUser.isPresent()) {
+      nameSystemUser = optionalSystemUser.get().getName();
     }
+    Assert.assertEquals(systemUser.getName(), nameSystemUser);
+    user.delete(id);
+  }
 
-    @Test
-    public void delete() {
-    }
-    @Test
-    public void findAll() {
-        List<SystemUser> userList = SystemUserDaoImpl.getInstance().findAll();
-        userList.forEach(System.out::println);
-    }
-    @Test
-    public void findById() {
-    }
+  @Test
+  public void delete() {
+  }
+
+  @Test
+  public void findAll() {
+    List<SystemUser> userList = SystemUserDaoImpl.getInstance().findAll();
+    Assert.assertNotNull(userList);
+  }
+
+  @Test
+  public void findById() {
+  }
 }
